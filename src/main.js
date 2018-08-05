73b25b652700efc6a73fcb8baea591bf3bfbc7f9
Jackson Parker <jack@rethinq.io>
2018-08-05 12:37:38 -0400

    [setup] Add Buefy for basic UI components

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from "vue";
+import Buefy from "buefy";
+import "buefy/lib/buefy.css";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
+
+// Configure Buefy
+// - https://buefy.github.io/#/documentation/constructor-options
+Vue.use(Buefy);
 
 new Vue({
   router,
